1cb24605faf27983c21d8b43caef3af8725106d4
yurasulima <yurasulima124@gmail.com>
2023-11-13 19:05:14 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.json:json:20231013")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    archiveFileName = "myname.jar"
+    manifest {
+        attributes["Main-Class"] = "org.example.Main"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+import org.json.JSONObject;
+
+public class Main {
+    public static void main(String[] args) {
+        printJson();
+    }
+
+    public static void printJson(){
+        JSONObject json = new JSONObject()
+                .put("name", "Yura")
+                .put("lastName", "Sulima");
+        System.out.println(json.toString());
+    }
+}
